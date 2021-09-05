50bc85bc6976b250b1be9d93a5534d089d2b9ea7
ymitsutomi-personal <79044843+ymitsutomi-personal@users.noreply.github.com>
2021-09-05 23:12:56 +0900

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -67,3 +67,17 @@ class CarDealer:
         return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+    def __init__(self, name, dealership, review, purchase, purchase_date, car_make, car_model, car_year, sentiment):
+        self.name = name
+        self.dealership = dealership
+        self.review = review
+        self.purchase = purchase
+        self.purchase_date = purchase_date
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year = car_year
+        self.sentiment = sentiment
+
+    def __str__(self):
+        return "Review: " + self.review
